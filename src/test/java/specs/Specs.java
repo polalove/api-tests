e8ac078074c8d1f415609dd66d13747d5eb3a064
Ivan Novikov <pola2288@yandex.ru>
2025-04-07 14:55:54 +0300

    add gtl files for allure

--- src/test/java/specs/Specs.java
+++ src/test/java/specs/Specs.java
@@ -1,10 +1,10 @@
 package specs;
 
-import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
@@ -12,7 +12,7 @@ import static io.restassured.http.ContentType.JSON;
 
 public class Specs {
     public static RequestSpecification request = with()
-            .filter(new AllureRestAssured())
+            .filter(withCustomTemplates())
             .log().uri()
             .log().body()
             .log().headers()
